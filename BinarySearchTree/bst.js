73062f080a3d76d59c78746fe33e804c694a6717
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-10 13:26:44 +0530

    binary search tree implementation done

---
+++ BinarySearchTree/bst.js
@@ -0,0 +1,15 @@
+// implementation bst
+class BSTNode{
+    constructor(key){
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null
+    }
+}
+
