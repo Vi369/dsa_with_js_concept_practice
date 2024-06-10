90226faaf8c104aab05deea1d89d2effdd5c7e63
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-10 15:23:40 +0530

    insertion in bts

--- BinarySearchTree/bst.js
+++ BinarySearchTree/bst.js
@@ -11,5 +11,55 @@ class BinarySearchTree{
     constructor(){
         this.root = null
     }
+
+    // insert(key){
+    //     const newNode = new BSTNode(key);
+    //     if(this.root === null){
+    //         this.root = newNode;
+    //     }else{
+    //         this.insertMethod(this.root, newNode)
+    //     }
+    // }
+
+    // insertMethod(node, newNode){ // node means this.root
+    //     if(newNode.key < node.key){
+    //         if(this.left === null){
+    //             this.left = newNode
+    //         }else(
+    //             this.insertMethod(this.left, newNode)
+    //         )
+    //     }else{
+    //         if(this.right === null){
+    //             this.right = newNode
+    //         }else{
+    //             this.insertMethod(this.right, newNode)
+    //         }
+    //     }
+    // }
+
+    insert(key){
+        const newNode = new BSTNode(key);
+        if(this.root === null){// it means this is the first value in bst
+            this.root = newNode;
+        }else{
+            this.insertMethod(this.root, newNode) 
+        }
+    }
+
+    insertMethod(node, newNode){
+        if(newNode.key < node.key){ // comparision between root value and newNode key value
+            if(node.left === null){
+                node.left = newNode
+            }else{
+                this.insertMethod(node.left, newNode) 
+            }
+        }else{ // means value is greater then root
+            if(node.right === null){
+                node.right = newNode
+            }else{
+                this.insertMethod(node.right, newNode)
+            }
+        }
+    }
 }
 
