1729e5f1a8b607c84e2b72c0f0b795f472a5e6dc
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-19 18:04:45 +0530

    graph itterative way usage example and bug fix

--- Graph/deapthFirstSearch.js
+++ Graph/deapthFirstSearch.js
@@ -1,22 +1,34 @@
 // deapth first traversal
 
+const graph = {
+    "a": ['b', 'c'],
+    "b": ['a', 'c', 'd'],
+    "c": ['a', 'b', 'd'],
+    "d": ['b', 'c'],
+    "e": ['f'],
+    "f": ['e']
+  };
 
 function deapthFirstSearchItterativeWay(graph, start){
     const stack = [start]
     let visited = new Set()
 
     while(stack.length > 0){
-        const StackPopCurrentNode = stack.pop()
-        if(!visited.has(StackPopCurrentNode)){
-            visited.add(StackPopCurrentNode)
-        }
-        for(const neighbour of graph[StackPopCurrentNode]){
-            if(!visited.has(neighbour)){
-                stack.push(neighbour)
+        const currentNode = stack.pop()
+        if(!visited.has(currentNode)){
+            visited.add(currentNode)
+            for(const neighbour of graph[currentNode]){
+                if(!visited.has(neighbour)){
+                    stack.push(neighbour)
+                }
             }
         }
     }
+    return visited
 }
+
+console.log(deapthFirstSearchItterativeWay(graph, 'a'))
+
 
 
 
