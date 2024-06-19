a145fef1ec234a751a0a40cdd12054e73740684c
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-19 19:17:30 +0530

    breadth first serach in graph

---
+++ Graph/BreadthFirstSearch.js
@@ -0,0 +1,27 @@
+// ex
+const graph = {
+    "a": ['b', 'c'],
+    "b": ['a', 'c', 'd'],
+    "c": ['a', 'b', 'd'],
+    "d": ['b', 'c'],
+    "e": ['f'],
+    "f": ['e']
+  };
+
+  function BreadthFirstSerach(graph, start, visited = new Set()){
+    const queue = [start]
+
+    while(queue.length > 0){
+        const node = queue.shift();
+
+        for(const neighbour of graph[node]){
+            if(!visited.has(neighbour)){
+                visited.add(neighbour)
+                queue.push(neighbour)
+            }
+        }
+    }
+    return visited;
+  }
+
+  console.log(BreadthFirstSerach(graph, 'a'))
