e5c7d5160fc933402d1014c38797a48d19a7537e
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-21 14:47:30 +0530

    tribonacci seriece with bottom up approach

--- problems/DynamicProgramming/fib_memoization.js
+++ problems/DynamicProgramming/fib_memoization.js
@@ -12,4 +12,28 @@ function fibMemo(n, memo = {}){
     return memo[n]
 }
 
-console.log(JSON.stringify(fibMemo(5)))
+// console.log(JSON.stringify(fibMemo(5)))
+
+
+// let count = 0
+// tribo 
+function tribMemo(n, memo = {}){
+    // count++
+    if(n in memo){
+        return memo[n]
+    }
+    if(n == 0){
+        return 0
+    }
+    if(n == 1 || n == 2){
+        return 1
+    }
+
+    memo[n]= tribMemo(n-1, memo) + tribMemo(n-2, memo) + tribMemo(n-3, memo)
+
+    return memo[n]
+}
+
+
+console.log(JSON.stringify(tribMemo(5)))
+// console.log(count)
