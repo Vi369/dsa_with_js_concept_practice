27c8d46f91f47d394f3cf558d57b48c32e01d5a2
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-22 20:23:44 +0530

    merge string alternative

---
+++ leetcode/mergeString.js
@@ -0,0 +1,29 @@
+var mergeAlternately = function(word1, word2) {
+    let mergedStr = ''
+    let i = 0;
+    let j = 0
+
+    while(i < word1.length && j< word2.length){
+        mergedStr += word1[i]
+        mergedStr += word2[j]
+        i++
+        j++
+    }
+
+    while(i< word1.length){
+        mergedStr +=word1[i]
+        i++
+    }
+    while(j<word2){
+        mergedStr +=word2[j]
+        j++
+    }
+
+    return mergedStr
+
+};
+
+let word1 = "abc"
+let word2 = "pqr"
+
+console.log(mergeAlternately(word1, word2))
