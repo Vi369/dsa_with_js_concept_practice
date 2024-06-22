071209ca1bf3a15da8c13ad84c2a944c720ce086
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-22 17:09:40 +0530

    usage example grid path counting

--- problems/DynamicProgramming/gridPathCounting.js
+++ problems/DynamicProgramming/gridPathCounting.js
@@ -29,10 +29,26 @@ function countPaths(grid, row = 0, col= 0, memo ={}){
 }
 
 
-// example 
+// Example 
 const grid = [
     [0,0],
     [0,0]
 ]
 
 console.log(countPaths(grid)) // expected output: 2 
+
+//Example 2
+const grid2 = [
+    [0, 0, 0],
+    [0,'X',0],
+    [0, 0, 0]
+];
+console.log(countPaths(grid2)); // Output: 2
+
+// example 3: 3x3 grid with more obstacles
+const grid3 = [
+    [0, 0, 0],
+    ['X','X',0],
+    [0, 0, 0]
+];
+console.log(countPaths(grid3)); // 1
