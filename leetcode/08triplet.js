c501c77ce6da874a4c15c343766fddadae19f100
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-24 19:59:20 +0530

    modified correct code bug fixes and usage example

--- leetcode/08triplet.js
+++ leetcode/08triplet.js
@@ -1,12 +1,29 @@
+// var increasingTriplet = function(nums) {
+//     let smallestValue = Infinity
+//     let secondSmallestValue = Infinity
+
+//     for(let i=0; i< nums.length; i++){
+//         if(nums[i] <= smallestValue){
+//             smallestValue = nums[i]
+//         }else if( nums[i+1] < secondSmallestValue){
+//             secondSmallestValue = nums[i+1]
+//         }else{
+//             return true
+//         }
+//     }
+//     return false
+// };
+
+
 var increasingTriplet = function(nums) {
     let smallestValue = Infinity
     let secondSmallestValue = Infinity
 
-    for(let i=0; i< nums.length; i++){
-        if(nums[i] <= smallestValue){
-            smallestValue = nums[i]
-        }else if( nums[i+1] < secondSmallestValue){
-            secondSmallestValue = nums[i+1]
+    for(const num of nums){
+        if(num <= smallestValue){
+            smallestValue = num
+        }else if( num <= secondSmallestValue){
+            secondSmallestValue = num
         }else{
             return true
         }
@@ -14,8 +31,14 @@ var increasingTriplet = function(nums) {
     return false
 };
 
-// dry run 
-// nums = [5,2,3,4,5]
+// exa1
+nums = [2,4,0,-2,-3]
+//exa 2
+nums1 = [2,4,-1,-2,-3]
 
-// smallestValue // 5
-// secondSmallestValue // na 
+// exa3 
+nums2 = [2,4,0,-1,1,2]
+
+console.log(increasingTriplet(nums)) // false
+console.log(increasingTriplet(nums1)) // false
+console.log(increasingTriplet(nums2)) // true
