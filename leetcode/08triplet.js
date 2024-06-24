c206630f64b0f387402e94539d30de5c3347bdca
Vikas singh varma <Vikashverma3001@gmail.com>
2024-06-24 19:35:54 +0530

    increasing triplet sequence

---
+++ leetcode/08triplet.js
@@ -0,0 +1,21 @@
+var increasingTriplet = function(nums) {
+    let smallestValue = Infinity
+    let secondSmallestValue = Infinity
+
+    for(let i=0; i< nums.length; i++){
+        if(nums[i] <= smallestValue){
+            smallestValue = nums[i]
+        }else if(secondSmallestValue < nums[i+1]){
+            secondSmallestValue = nums[i+1]
+        }else{
+            return true
+        }
+    }
+    return false
+};
+
+// dry run 
+// nums = [5,2,3,4,5]
+
+// smallestValue // 5
+// secondSmallestValue // na 
