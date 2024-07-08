34960d38478c983f9a470534fd4315ec53d621d6
Vikas singh varma <Vikashverma3001@gmail.com>
2024-07-08 16:11:51 +0530

    queue complete revision and real-world ex

--- Revision/Queue/queue.js
+++ Revision/Queue/queue.js
@@ -34,7 +34,7 @@ Queue.prototype.size = function(){
 
 // print queue
 Queue.prototype.print = function(){
-    let print = []
+    let print = ''
     if (this.isEmpty()){
         return print // empty array or message 
     }
@@ -45,10 +45,11 @@ Queue.prototype.print = function(){
 }
 
 // isEmapty
-Queue.prototype.isEmpty = function(data){
+Queue.prototype.isEmpty = function(){
     return this.queue.length === 0
 }
 
+/** 
 // example 
 const myQueue = new Queue();
 
@@ -70,5 +71,42 @@ console.log("Removed element:", myQueue.dequeue()); // Output: true
 console.log("Is the queue empty? ", myQueue.isEmpty()); // Output: false
 console.log("Element in Queue:",myQueue.print()); //
 
+*/
 
-// example 2 
+/** example 2 
+ * task scheduler
+ *  */ 
+
+class TaskScheduler{
+    constructor(){
+        this.task = new Queue()
+    }
+
+    addTask(task){
+        this.task.enqueue(task);
+    }
+
+    executeTask(){
+        this.task.dequeue();
+    }
+
+    printPendingTask(){
+        if(this.task.isEmpty()){
+            return 'No task Pending!'
+        }else{
+            return this.task.print()
+        }
+    }
+}
+
+const schedule = new TaskScheduler();
+
+schedule.addTask('Play first song!')
+schedule.addTask('Play second song!')
+schedule.addTask('Play third song!')
+
+schedule.executeTask();
+
+console.log(schedule.printPendingTask());
+
+// console.log(schedule)
